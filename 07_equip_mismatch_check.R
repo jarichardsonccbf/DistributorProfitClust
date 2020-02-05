41d1f2b5cf40059774c5b90079a2126dd8aad344
Jason Richardson <jarichardson@cocacolaflorida.com>
2020-02-05 14:22:40 -0500

    fixed counting issue on 04_equip (n() to sum(field)
    04_2_equipment added to get total equip opex regardless of equipment class
    
    07_equip_mismatch_check added to get outlets with equipment they shouldnt have.

---
+++ 07_equip_mismatch_check.R
@@ -0,0 +1,32 @@
+source("05_oi_oiratio.R")
+
+mm.check <- rev.opex %>% 
+  ungroup() %>% 
+  select(`Distributor Name`, `Customer Name`, `Account Number`, `LMP Volume`, `RTM Volume`, `Relevant Equipment Count`, `Postmix Equip Count`, `Cooler Count`, `Vending Equip Count`)
+
+rm(rev.opex)
+
+mm.check <- mm.check %>% 
+  filter(`Relevant Equipment Count` != 0 |
+           `Postmix Equip Count` != 0 |
+           `Cooler Count` != 0 |
+           `Vending Equip Count` != 0)
+
+mm.check.lmp <- mm.check %>% 
+  filter(`RTM Volume` == 0 &
+           `LMP Volume` > 0) %>% 
+  filter(`Cooler Count` > 0 |
+           `Vending Equip Count` > 0)
+
+mm.check.bc <- mm.check %>% 
+  filter(`RTM Volume` > 0 &
+           `LMP Volume` == 0) %>% 
+  filter(`Postmix Equip Count` > 0)
+
+library(xlsx)
+
+# write.xlsx(as.data.frame(mm.check.lmp), file="deliverables/equipment_checks.xlsx", sheetName="sheet1", row.names=FALSE)
+
+# write.xlsx(as.data.frame(mm.check.bc), file="deliverables/equipment_checks.xlsx", sheetName="sheet2", append=TRUE, row.names=FALSE)
+
+# semijoin with mm for mm.check.bc
